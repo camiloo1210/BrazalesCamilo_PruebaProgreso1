16a47dffa743c2876973c62d846c58e399066971
laboratorio <laboratorio@UPOAULA10302>
2024-10-30 08:19:46 -0500

    Agregar archivos de proyecto.

---
+++ BrazalesCamilo_PruebaProgreso1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BrazalesCamilo_PruebaProgreso1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
