96d4ab50dc7acc2d0470c3e07d5c2443c0c9d817
SejoLape123 <camilobrazales@gmail.com>
2024-10-30 18:25:04 -0500

    Configuracion UI Usuarios

---
+++ BrazalesCamilo_PruebaProgreso1/Migrations/20241030230103_Migracion 3.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BrazalesCamilo_PruebaProgreso1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migracion3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
