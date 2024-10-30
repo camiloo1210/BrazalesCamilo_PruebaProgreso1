5712ac88e058aae790231dd3daaa5f203f74f25b
camiloo <camiloandresbrazalesbarrero@gmail.com>
2024-10-30 08:45:44 -0500

    Creacion modelos completos

---
+++ BrazalesCamilo_PruebaProgreso1/Models/CBrazales.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BrazalesCamilo_PruebaProgreso1.Models
+{
+    public class CBrazales
+    {
+        [Key]
+        public int Id_usuario { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Nombre { get; set; }
+        [Required]
+        [DisplayName("Tiene trabajo")]
+        public bool Tiene_trabajo { get; set; }
+        [Required]
+        public DateTime Ingreso { get; set; }
+        [Required]
+        public double Salario   { get; set; }
+        public Celular? Celular { get; set; }
+        [Required]
+        [ForeignKey("Celular")]
+        public int Id_celular { get; set; }
+
+    }
+}
---
+++ BrazalesCamilo_PruebaProgreso1/Models/Celular.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BrazalesCamilo_PruebaProgreso1.Models
+{
+    public class Celular
+    {
+        [Key]
+        public int Id_celular { get; set; }
+        [Required]
+        [DisplayName("Modelo")]
+        public string CelularNombre { get; set; }
+        [Required]
+        public int anio { get; set; }
+        [Required]
+        public double precio { get; set; }
+    }
+}
