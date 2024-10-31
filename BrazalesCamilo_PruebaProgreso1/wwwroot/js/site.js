b36d15bce137ab6d9e1a1cd1ad5461b35fc48cc4
SejoLape123 <camilobrazales@gmail.com>
2024-10-30 19:06:03 -0500

    Creacion de alerta en el boton de inicio;Profe una cosa SejoLape123 es mi otra cuenta jsjs

--- BrazalesCamilo_PruebaProgreso1/wwwroot/js/site.js
+++ BrazalesCamilo_PruebaProgreso1/wwwroot/js/site.js
@@ -1,4 +1,6 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-
+function InformacionCamiloBrazales() {
+    alert("Mi nombre es:Camilo Brazales\n Mi hobbie es:Jugar Valorant");
+}
 // Write your JavaScript code.
